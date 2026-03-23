07fb2ad222712e24bd0c7afc1252b36dc2a27fd8
JorgeSB <jsarriab28@gmail.com>
2026-03-23 19:19:27 +0100

    Initial Commit

---
+++ Braver/BraverApp.swift
@@ -0,0 +1,17 @@
+//
+//  BraverApp.swift
+//  Braver
+//
+//  Created by Jorge Sarria Bruned on 23/3/26.
+//
+
+import SwiftUI
+
+@main
+struct BraverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Braver/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Braver
+//
+//  Created by Jorge Sarria Bruned on 23/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
